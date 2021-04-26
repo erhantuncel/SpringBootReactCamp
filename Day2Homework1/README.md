6a3bb2a5da7ff090463c137c6fbaeefd178e97dc
Erhan TUNCEL <erhan.glstrme@gmail.com>
2021-04-26 23:46:54 +0300

    Added: Day2Homework1

---
+++ Day2Homework1/README.md
@@ -0,0 +1,9 @@
+# Day 2 Homework
+
+## Property Classes
+1. Lecture
+2. Comment
+
+## Business Classes
+1. LectureManager
+2. CommentManager
---
+++ Day2Homework1/src/LectureManager.java
@@ -0,0 +1,9 @@
+public class LectureManager {
+    public void add(Lecture lecture) {
+        System.out.println("Ders eklendi: " + lecture.getName() + "(" + lecture.getSubject() + ")");
+    }
+
+    public void delete(Lecture lecture) {
+        System.out.println("Ders silindi: " + lecture.getName() + "(" + lecture.getSubject() + ")");
+    }
+}
