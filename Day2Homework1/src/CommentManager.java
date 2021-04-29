81de5aabde7d2ae393534d3b4ab858f7348ed91f
Erhan TUNCEL <erhan.glstrme@gmail.com>
2021-04-29 17:22:06 +0300

    Deleted: unnecessary files.

--- Day2Homework1/src/CommentManager.java
+++
@@ -1,9 +0,0 @@
-public class CommentManager {
-    public void add(Comment comment) {
-        System.out.println("Yorum eklendi No: " + comment.getId());
-    }
-
-    public void delete(Comment comment) {
-        System.out.println("Yorum silindi No: " + comment.getId());
-    }
-}
--- Day2Homework1/src/LectureManager.java
+++
@@ -1,9 +0,0 @@
-public class LectureManager {
-    public void add(Lecture lecture) {
-        System.out.println("Ders eklendi: " + lecture.getName() + "(" + lecture.getSubject() + ")");
-    }
-
-    public void delete(Lecture lecture) {
-        System.out.println("Ders silindi: " + lecture.getName() + "(" + lecture.getSubject() + ")");
-    }
-}
